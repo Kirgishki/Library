98abb9383f36cc8490f9b5ebcabf0eea7c35e0ed
Zika Linux <115041465+Kirgishki@users.noreply.github.com>
2024-08-02 16:55:46 +0200

    Change style

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
-const myLibrary = [new Book("Zika"), new Book("Slika")];
+const myLibrary = [new Book("Witcher: Taj i taj da ga ne bi videodwadaw"), new Book("Song of Ice and Fire")];
 
 const addBooksButton = document.querySelector("#addBookButton");
+const importBookButton = document.querySelector("#importBookButton");
 const addBookDialog = document.querySelector("#addBookDialog");
+const addBookForm = document.querySelector("#addBookDialog > form");
 const cancelAddBookButton = document.querySelector("#cancelAddBookButton");
 
-console.log(addBooksButton);
 
-function Book(name) {
+function Book(name, author, pages) {
     this.name = name;
-  }
-  
+    this.author = author;
+    this.pages = pages;
+}
+
+Book.prototype.addBookToLib = function(){
+    myLibrary.push(this);
+}
+
 function addBookToLibrary() {
 let newBook = new Book(prompt("Enter the book name: "));
 myLibrary.push(newBook);
@@ -18,13 +25,22 @@ myLibrary.push(newBook);
 function displayBooks(){
     
     let booksDiv = document.querySelector(".booksContainer");
+    booksDiv.textContent = "";
     
     myLibrary.forEach(book => {
         let card = document.createElement("div");
+        card.classList.add('bookCard');
+        let bookMenu = document.createElement("div");
+        bookMenu.classList.add('bookMenu');
+        bookMenu.innerHTML = `<ul>
+            <li>Remove<li>
+            <li>Read<li>
+        </ul>`;
         let bookTitle = document.createElement("h3");
         
         bookTitle.textContent = book.name;
         card.appendChild(bookTitle);
+        card.appendChild(bookMenu);
         booksDiv.appendChild(card);
     });
     
@@ -37,7 +53,17 @@ addBooksButton.addEventListener("click", () => {
     addBookDialog.showModal();
 });
 
-cancelAddBookButton.addEventListener("click", (e)=>{
-    e.preventDefault();
-    addBookDialog.showModal();
+addBookDialog.addEventListener("close", (e)=>{
+    
 });
+
+importBookButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let newBookInfo = Object.values(addBookForm).reduce((obj,field) => { obj[field.name] = field.value; return obj }, {});
+    let newBook = new Book(newBookInfo.bookName, newBookInfo.bookAuthor, newBookInfo.numOfPages);
+    newBook.addBookToLib();
+    displayBooks();
+
+    addBookDialog.close();
+});
+
